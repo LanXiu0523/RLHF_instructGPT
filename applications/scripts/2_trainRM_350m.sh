c67a36d4b48245dc849fea4d4bf25efde090a27d
LanXiu-0523 <915036614@qq.com>
2023-07-19 15:02:04 +0800

    update

--- applications/scripts/2_trainRM_350m.sh
+++ applications/scripts/2_trainRM_350m.sh
@@ -15,11 +15,19 @@ mkdir -p $OUTPUT $OUTPUT/2_model_RM $OUTPUT/logs
 
 deepspeed --num_gpus 1 applications/train/2_trainRM.py \
     --data_path  'datafile/Dahoas/rm-static' \
+    --data_split 2,4,4 \
     --model_name_or_path facebook/opt-350m \
-    --num_padding_at_beginning 1 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 16 \
+    --max_seq_len 512 \
+    --learning_rate 5e-5 \
     --weight_decay 0.1 \
+    --num_train_epochs 1 \
     --disable_dropout \
     --gradient_accumulation_steps 4 \
+    --lr_scheduler_type cosine \
+    --num_warmup_steps 0 \
+    --seed 1234 \
     --zero_stage $ZERO_STAGE \
     --deepspeed \
     --output_dir $OUTPUT/2_model_RM \
