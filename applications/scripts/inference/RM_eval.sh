409e0d0d2c4bfc493eb96fccb6d35d2bcc5419b7
LanXiu-0523 <915036614@qq.com>
2023-08-14 05:52:43 +0000

    update

--- applications/scripts/inference/RM_eval.sh
+++ applications/scripts/inference/RM_eval.sh
@@ -7,6 +7,6 @@ if [ "$OUTPUT" == "" ]; then
 fi
 
 
-python applications/inference/RM_eval.py \
+python3 applications/inference/RM_eval.py \
     --model_name_or_path 'output/2_model_RM' \
     2>&1 | tee $OUTPUT/logs/RM_eval.log
